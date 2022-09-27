5ea52194cbba3c935525aa70e77b2c1ca1734bdf
allennnnnnnnnn <112918683+allennnnnnnnnn@users.noreply.github.com>
2022-09-27 12:05:40 +0800

    week04

---
+++ week04-5迴圈.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    for(int i=0; i<4; i++) {
+        printf("出現 i:%d\n",i);
+    }
+    for(int i=1; i<=4; i++) {
+        printf("接下來 i:%d\n",i);
+
+    }
+
+
+
+}
+
