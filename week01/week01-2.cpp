3340ff43f574d6405deaa92c18035a17c734d1ff
allennnnnnnnnn <xbshzz03@gmail.com>
2022-11-08 12:06:37 +0800

    修改目錄

---
+++ week01/week01-2.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+
+{
+    printf("Hello World\n");
+    printf("Hello World\n");
+    printf("我是黃智遠\n");
+    ///有時候中文會有問題,你可用英文
+    //註解 comments 是給別人看的 作業2加註解
+    ///Codeblocks如果畫3條,會比較深
+}
